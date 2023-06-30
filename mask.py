62b02aa4f77727208b4c65016db161cfc47ae62a
Norah Martin <93730785+Norah-Martin@users.noreply.github.com>
2023-06-30 19:58:58 +0530

    Add files via upload

---
+++ mask.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+import random
+
+
+
+train_path = "C:\Users\ASUS\Desktop\python\project2\face_mask_detection_yolov5\face_data\train"
+val_path = "C:\Users\ASUS\Desktop\python\project2\face_mask_detection_yolov5\face_data\val"
+test_path ="C:\Users\ASUS\Desktop\python\project2\test" 
